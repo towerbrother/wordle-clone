8f3d378a32d15594dc746e58b0bbcf4374640643
GTO <giorgio.torre@stellenanzeigen.de>
2022-03-24 07:36:38 +0100

    added keyboard

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-console.log("hello wordle");
+const tiles = document.querySelector(".tiles");
+const keyboard = document.querySelector(".keyboard");
+
+const keys = [
+  "Q",
+  "W",
+  "E",
+  "R",
+  "T",
+  "Y",
+  "U",
+  "I",
+  "O",
+  "P",
+  "A",
+  "S",
+  "D",
+  "F",
+  "G",
+  "H",
+  "J",
+  "K",
+  "L",
+  "ENTER",
+  "Z",
+  "X",
+  "C",
+  "V",
+  "B",
+  "N",
+  "M",
+  "<<",
+];
+
+const handleButtonClick = () => {
+  console.log("button clicked");
+};
+
+keys.forEach((key) => {
+  const button = document.createElement("button");
+  button.textContent = key;
+  button.setAttribute("id", key);
+  button.addEventListener("click", handleButtonClick);
+  keyboard.append(button);
+});
